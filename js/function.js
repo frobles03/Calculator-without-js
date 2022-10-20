084afda079eceeec9be802e385e83c1ceb5a6c0f
frobles03 <f.robles03@fromail.cl>
2022-10-20 00:41:08 -0300

    se corrigio el error

--- js/function.js
+++ js/function.js
@@ -136,6 +136,7 @@ function obtener(){
       case "/":
         res = parseFloat(opc1) / parseFloat(opc2);
         mostrarNumF.textContent=opc1+operacion+ opc2  + "="+res;
+        mostrarOperacionF.textContent="";
          break;
     }
     
